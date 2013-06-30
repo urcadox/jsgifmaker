a612f3d43025905a403eb6b518a64aa9e0b93e3d
Alexandre Berthaud <alexandre@berthaud.me>
2013-06-30 20:45:24 +0200

    Add: Check for File API support

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 var JSGifMaker = {
   init: function() {
+    this.fileApiSupportCheck();
+  },
+
+  fileApiSupportCheck: function() {
+    if(!(window.File && window.FileReader && window.FileList && window.Blob)) {
+      $(".alert.fileapinotsupported").show();
+    }
   }
 };
 JSGifMaker.init();
