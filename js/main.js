c03b9a9cceb8816d566d50a57452021f7390dd72
Alexandre Berthaud <alexandre@berthaud.me>
2014-03-28 17:03:00 +0100

    Add: From video

--- js/main.js
+++ js/main.js
@@ -2,9 +2,10 @@ var JSGifMaker = {
   init: function() {
     var that = this;
     this.fileApiSupportCheck();
-    $(".select-images input").change(_.bind(this.imagesSelectedHandler, this));
+    $(".select-images input[type=file]").change(_.bind(this.imagesSelectedHandler, this));
+    $(".select-video input[type=file]").change(_.bind(this.videoSelectedHandler, this));
     $(".controls .generate").click(_.bind(this.generate, this));
-    $(".controls input[type=range]").change(function() {
+    $(".controls input[type=range], .select-video input[type=range]").change(function() {
       $(this).prev().find(".value").text(this.value);
     });
     $(".controls .fps").change(function() {
@@ -12,11 +13,16 @@ var JSGifMaker = {
         that.setRefreshInterval(this.value);
       }
     });
+    $(".select-video input[type=range]").change(function() {
+      that.videoFPS = this.value;
+    });
+    $(".select-video .extract-frames").click(_.bind(this.startExtractingFrames, this));
   },
 
   canvases: [],
 
   imagesSelectedHandler: function(e) {
+    $(".select-video").css("opacity", 0.3).find("input").attr("disabled", "disabled");
     var list = e.target.files, that = this;
     for(var i = 0; i < list.length; i++) {
       var img = new Image();
@@ -51,6 +57,18 @@ var JSGifMaker = {
     this.setRefreshInterval(parseInt($(".controls .fps").val()));
   },
 
+  videoSelectedHandler: function(e) {
+    $(".select-images").css("opacity", 0.3).find("input").attr("disabled", "disabled");
+    var file = e.target.files[0], that = this;
+    if(typeof file != "undefined") {
+      this.video = document.createElement("video");
+      this.video.src = URL.createObjectURL(file);
+      this.videoFPS = $(".select-video input[type=range]").val();
+      $(".select-video .parameters").show();
+      $(this.video).appendTo($(".preview"));
+    }
+  },
+
   setRefreshInterval: function(fps) {
     if(typeof this.previewInterval != "undefined") {
       clearInterval(this.previewInterval);
@@ -68,6 +86,49 @@ var JSGifMaker = {
     this.canvases[current].style.visibility = "visible";
   },
 
+  stopPreview: function() {
+    clearInterval(this.previewInterval);
+  },
+
+  startExtractingFrames: function() {
+    this.canvases = [];
+    this.video.onseeked = _.bind(this.extractFrame, this);
+    _.bind(this.extractFrame, this)();
+  },
+
+  extractFrame: function() {
+    var last = _.last(this.canvases);
+    var current = document.createElement("canvas");
+    current.width = this.video.videoWidth;
+    current.height = this.video.videoHeight;
+    current.getContext("2d").drawImage(this.video, 0, 0, current.width, current.height);
+    if(typeof last == "undefined" || !this.equalCanvases(last, current)) {
+      this.canvases.push(current);
+    }
+    if(this.video.ended) {
+      this.imagesLoadedHandler();
+    } else {
+      this.videoNextFrame();
+    }
+  },
+
+  videoNextFrame: function() {
+    this.video.currentTime += 1 / this.videoFPS;
+  },
+
+  equalCanvases: function(c1, c2) {
+    var data1 = c1.getContext("2d").getImageData(0, 0, c1.width, c1.height).data;
+    var data2 = c2.getContext("2d").getImageData(0, 0, c2.width, c2.height).data;
+
+    var i = 0;
+    var equal = true;
+    while(i <= data1.length && equal) {
+      equal = data1[i] == data2[i];
+      i++;
+    }
+    return equal;
+  },
+
   fileApiSupportCheck: function() {
     if(!(window.File && window.FileReader && window.FileList && window.Blob)) {
       $(".alert.fileapinotsupported").show();
@@ -75,6 +136,8 @@ var JSGifMaker = {
   },
 
   generate: function() {
+    this.stopPreview();
+
     var gif = new GIF({
       workers: 4,
       quality: parseInt($(".controls .quality").val()),
